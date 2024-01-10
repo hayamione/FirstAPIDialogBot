6e03f53579aefddb73c0175fd6ee75c183533d94
hayamione <haya.zubiar545@gmail.com>
2024-01-10 17:08:43 +0530

    update bot

---
+++ Dialogs/GetUserDetailsDialog.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Bot.Builder.Dialogs.Choices;
+using System.Collections.Generic;
+
+namespace NewDialogBot.Dialogs
+{
+    public class GetUserDetailsDialog : ComponentDialog
+    {
+        // Define value names for values tracked inside the dialogs.
+        private const string UserInfo = "value-userInfo";
+
+        public class DatePrompt : TextPrompt
+        {
+            public DatePrompt(string dialogId, PromptValidator<string> validator = null)
+                : base(dialogId, validator)
+            {
+            }
+        }
+
+        public class PostalCodePrompt : NumberPrompt<int>
+        {
+            public PostalCodePrompt(string dialogId, PromptValidator<int> validator = null)
+                : base(dialogId, validator)
+            {
+            }
+        }
+
+        public GetUserDetailsDialog() : base(nameof(GetUserDetailsDialog))
+        {
+            // This array defines how the Waterfall will execute.
+            var waterfallSteps = new WaterfallStep[]
+            {
+                NameStepAsync,
+                FamilyNameStepAsync,
+                FullNameStepAsync,  
+                BirthDateStepAsync,
+                GenderPromptStepAsync,
+                PostalCodeStepAsync,
+                ExitStepAsync
+                //SummaryStepAsync,
+            };
+
+            // Add named dialogs to the DialogSet. These names are saved in the dialog state.
+            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), waterfallSteps));
+
+            AddDialog(new TextPrompt(nameof(TextPrompt)));
+            AddDialog(new NumberPrompt<int>(nameof(NumberPrompt<int>)));
+            AddDialog(new ChoicePrompt(nameof(ChoicePrompt)));
+
+            AddDialog(new PostalCodePrompt(nameof(PostalCodePrompt), PostalCodePromptValidatorAsync));
+            AddDialog(new DatePrompt(nameof(DatePrompt), DatePromptValidatorAsync));
+
+            // The initial child Dialog to run.
+            InitialDialogId = nameof(WaterfallDialog);
+        }
+
+        private async Task<DialogTurnResult> NameStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            stepContext.Values[UserInfo] = new UserProfile();
+            return await stepContext.PromptAsync(
+                nameof(TextPrompt),
+                new PromptOptions { Prompt = MessageFactory.Text("Let's get started. Please provide your first name.") },
+                cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> FamilyNameStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var userProfile = (UserProfile)stepContext.Values[UserInfo];
+            userProfile.Given = (string)stepContext.Result;
+            return await stepContext.PromptAsync(
+                nameof(TextPrompt),
+                new PromptOptions { Prompt = MessageFactory.Text("Now, please provide your last / family name.") },
+                cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> FullNameStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var userProfile = (UserProfile)stepContext.Values[UserInfo];
+            userProfile.Family = (string)stepContext.Result;
+            return await stepContext.PromptAsync(
+                nameof(TextPrompt),
+                new PromptOptions { Prompt = MessageFactory.Text("Great, Now, please provide your full name.") },
+                cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> BirthDateStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var userProfile = (UserProfile)stepContext.Values[UserInfo];
+            userProfile.Name = (string)stepContext.Result;
+            return await stepContext.PromptAsync(
+                nameof(DatePrompt),
+                new PromptOptions { Prompt = MessageFactory.Text("Now, please provide your birth date."),
+                RetryPrompt = MessageFactory.Text("The birthdate should be in this format (yyyy-mm-dd). Try again.")
+                },
+                cancellationToken);
+
+        }
+
+        private static async Task<DialogTurnResult> GenderPromptStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var userProfile = (UserProfile)stepContext.Values[UserInfo];
+            userProfile.BirthDate = (string)stepContext.Result;
+            return await stepContext.PromptAsync(nameof(ChoicePrompt),
+                new PromptOptions
+                {
+                    Prompt = MessageFactory.Text("Please select your gender"),
+                    Choices = ChoiceFactory.ToChoices(new List<string> { "Male", "Female", "Other" }),
+                }, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> PostalCodeStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var userProfile = (UserProfile)stepContext.Values[UserInfo];
+            userProfile.Gender = ((FoundChoice)stepContext.Result).Value;
+            return await stepContext.PromptAsync(
+                nameof(PostalCodePrompt),
+                new PromptOptions
+                {
+                    Prompt = MessageFactory.Text("Lastly, please provide your postal code."),
+                    RetryPrompt = MessageFactory.Text("The postal code must be of 5 digits. Try again."),
+                },
+                cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> ExitStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var userProfile = (UserProfile)stepContext.Values[UserInfo];
+            userProfile.AddressPostalcode = (int)stepContext.Result;
+            return await stepContext.EndDialogAsync(stepContext.Values[UserInfo], cancellationToken);
+        }
+
+        private Task<bool> PostalCodePromptValidatorAsync(PromptValidatorContext<int> promptContext, CancellationToken cancellationToken)
+        {
+            // Validate if the entered number is a valid Indian phone number
+            return Task.FromResult(promptContext.Recognized.Succeeded && promptContext.Recognized.Value > 20000);
+        }
+
+        private Task<bool> DatePromptValidatorAsync(PromptValidatorContext<string> promptContext, CancellationToken cancellationToken)
+        {
+            string datePattern = @"^\d{4}-\d{2}-\d{2}$";
+            bool isValidDate = Regex.IsMatch(promptContext.Recognized.Value, datePattern);
+            return Task.FromResult(isValidDate);
+        }
+    }
+}
---
+++ Dialogs/MainDialog.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Choices;
+using Microsoft.Bot.Schema;
+using NewDialogBot;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace NewDialogBot.Dialogs
+{
+    public class MainDialog : ComponentDialog
+    {
+        private readonly IStatePropertyAccessor<UserProfile> _userState;
+        //private readonly JObject responses;
+
+        public MainDialog(UserState userState)
+            : base(nameof(MainDialog))
+        {
+            _userState = userState.CreateProperty<UserProfile>("UserProfile");
+
+            // This array defines how the Waterfall will execute.
+            var waterfallSteps = new WaterfallStep[]
+            {
+                WelcomeStepAsync,
+                HelpStartAsync,
+                SummarynApiStepAsync
+            };
+
+            // Add named dialogs to the DialogSet. These names are saved in the dialog state.
+            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), waterfallSteps));
+
+            AddDialog(new GetUserDetailsDialog());
+
+            // The initial child Dialog to run.
+            InitialDialogId = nameof(WaterfallDialog);
+        }
+
+        private async Task<DialogTurnResult> WelcomeStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var intentsJson = File.ReadAllText("Resources/intents.json");
+            var intents = JsonConvert.DeserializeObject<Dictionary<string, List<string>>>(intentsJson);
+
+            // Process user input
+            var userInput = stepContext.Context.Activity.Text;
+
+            // LUIS (CLU)
+
+            //Intent - DownloadEMR
+            //Entity - 
+            //"EMR", "Medical Record", "Get my EMR", "Get my medical record", "I need my EMR document", "I need my emr", "I need my medical record",
+            // "My name is Haya and am looking for my medical records and my gender is female."
+
+            // Name, DOB, gender and zip code
+            //YYYY-MM-DD My dob is if i rembeer correct 2000/09/09
+
+            switch (userInput)
+            {
+                case "greetingintent":
+                    await stepContext.Context.SendActivityAsync(MessageFactory.Text($"Hello! I'm your bot. Please type your request"), cancellationToken);
+                    break;
+
+                case var _ when intents["whoAreYou"].Contains(userInput):
+                    await stepContext.Context.SendActivityAsync(MessageFactory.Text($"I am Elite Bot. I display patient EMR."), cancellationToken);
+                    break;
+
+                case var _ when intents["help"].Contains(userInput):
+                    await stepContext.Context.SendActivityAsync(MessageFactory.Text($"Please type 'EMR or I need EMR' If you want EMR."), cancellationToken);
+                    //await stepContext.BeginDialogAsync(nameof(GetUserDetailsDialog), null, cancellationToken);
+                    break;
+
+                case var _ when intents["record"].Any(intent => userInput.Contains(intent)):
+                    await stepContext.Context.SendActivityAsync(MessageFactory.Text($"You entered EMR."), cancellationToken);
+                    // If user requests EMR, begin the GetUserDetailsDialog
+                    //await stepContext.BeginDialogAsync(nameof(GetUserDetailsDialog), null, cancellationToken);
+                    break;
+
+                default:
+                    await stepContext.Context.SendActivityAsync(MessageFactory.Text($"Sorry, I did not recognize your request. Try again!"), cancellationToken);
+                    break;
+            }
+            return await stepContext.EndDialogAsync(null, cancellationToken);
+        }
+
+
+        private async Task<DialogTurnResult> HelpStartAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            return await stepContext.BeginDialogAsync(nameof(GetUserDetailsDialog), null, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> SummarynApiStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var userInfo = (UserProfile)stepContext.Result;
+
+            if (userInfo != null)
+            {
+                var summaryMessage = $"TThank you! Here is the summary:\n\nGiven: {userInfo.Given}\n\nFamily: {userInfo.Family}\n\nName: {userInfo.Name}\n\nBirthdate: {userInfo.BirthDate}\n\nGender: {userInfo.Gender}\n\nAddress Postal Code: {userInfo.AddressPostalcode}";               
+                await stepContext.Context.SendActivityAsync(MessageFactory.Text(summaryMessage), cancellationToken);
+
+                // Make API call and display PDF
+                var apiResponse = await CallApiAsync(userInfo);
+                if (apiResponse != null)
+                {
+                    // Display PDF document or handle it as needed
+                    // Generate PDF and send it to the user
+                    var pdfStream = new MemoryStream(apiResponse);
+                    await SendPdfAsync(stepContext.Context, apiResponse);
+                }
+                else
+                {
+                    await stepContext.Context.SendActivityAsync(MessageFactory.Text("Failed to generate PDF, as API Response is null"), cancellationToken);
+                }
+
+                //var summaryMessage = $"We are in API Call Step in Waterfall";
+                //await stepContext.Context.SendActivityAsync(MessageFactory.Text(summaryMessage), cancellationToken);
+
+            }
+
+            else
+            {
+                await stepContext.Context.SendActivityAsync(MessageFactory.Text("PDF generation canceled, as User Info is empty."), cancellationToken);
+            }
+
+            return await stepContext.EndDialogAsync(null, cancellationToken);
+        }
+
+        private async Task<byte[]> CallApiAsync(UserProfile userInfo)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var apiUrl = "https://elitedevutilities.azurewebsites.net/api/DevUtilities/DownloadEMR";
+
+                // Modify payload to match the API requirements
+                /*var payload = new
+                {
+                    Given = userInfo.Name,
+                    Family = userInfo.Family,
+                    Name = userInfo.Name,
+                    BirthDate = userInfo.BirthDate,
+                    Gender = userInfo.Gender,
+                    AddressPostalcode = userInfo.AddressPostalcode
+                };*/
+
+                var payload = new
+                {
+                    Given = "Haya",
+                    Family = "Ahmad",
+                    Name = "Haya Zubair Ahmad",
+                    BirthDate = "1952-02-09",
+                    Gender = "Female",
+                    AddressPostalcode = "22042"
+                };
+
+                var response = await httpClient.PostAsJsonAsync(apiUrl, payload);
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseString = await response.Content.ReadAsByteArrayAsync();
+                    //File.WriteAllBytes("D:\workElite\bot-practice\GeneratedPdf.pdf", responseString);
+                    Console.WriteLine($"API Response: {responseString}");
+                    return responseString;
+                }
+                // Handle API call failure
+                return null;
+            }
+        }
+
+        private async Task SendPdfAsync(ITurnContext turnContext, byte[] pdfBytes)
+        {
+            if (pdfBytes != null && pdfBytes.Length > 0)
+            {
+                // Convert the byte array to a base64-encoded string
+                string base64Pdf = Convert.ToBase64String(pdfBytes);
+
+                // Create a Microsoft.Bot.Schema.Attachment with base64-encoded content
+                var pdfAttachment = new Microsoft.Bot.Schema.Attachment
+                {
+                    Name = "GeneratedPdf.pdf",
+                    ContentType = "application/pdf",
+                    ContentUrl = "data:application/pdf;base64," + base64Pdf,
+                };
+
+                var reply = turnContext.Activity.CreateReply();
+                reply.Attachments = new List<Microsoft.Bot.Schema.Attachment> { pdfAttachment };
+
+                await turnContext.SendActivityAsync(reply);
+            }
+            else
+            {
+                await turnContext.SendActivityAsync(MessageFactory.Text("Failed to generate or retrieve the PDF content."), CancellationToken.None);
+            }
+        }
+
+
+
+
+
+    }
+}
